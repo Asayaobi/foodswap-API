ad23433670914f5cf132824cb6149b369eb56e24
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 23:35:50 +0200

    Add get request to images table

--- routes/imagesRoute.js
+++ routes/imagesRoute.js
@@ -1,9 +1,17 @@
 import { Router } from 'express'
+import db from '../db.js'
 const router = Router()
 
 // Define a GET route for fetching the list of images
-router.get('/images', (req, res) => {
-  res.send('List of images')
+router.get('/images', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM images')
+    console.log('rows images', rows)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 // Export the router
