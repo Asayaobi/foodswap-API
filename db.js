b2baf5a5d188f6d63b5fc642250bd0a159bf8e66
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 23:16:23 +0200

    config env

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 // db.js
 import pg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config() // Load environment variables
+
 const { Pool } = pg
 const DBURL = process.env.DBURL
-
 // Database connection parameters
 const db = new Pool({
   ssl: {
