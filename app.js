1eb784b5a2c8051b2997be9cfa3f7dd1d4f7871d
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 22:13:13 +0200

    Import images routes to app

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ import usersRouter from './routes/usersRoutes.js'
 import foodRouter from './routes/foodRoutes.js'
 import bookingsRouter from './routes/bookingsRoute.js'
 import reviewsRouter from './routes/reviewsRoute.js'
+import imagesRouter from './routes/imagesRoute.js'
 
-// Tell the app to use the user router
+// Tell the app to use the imported routers
 app.use(usersRouter)
 app.use(foodRouter)
 app.use(bookingsRouter)
 app.use(reviewsRouter)
+app.use(imagesRouter)
 
 // create routes
 app.get('/', (req, res) => {
