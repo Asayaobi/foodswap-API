8df06b5460c8bbe9f5bbd78398da7ec9594a8302
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 22:07:10 +0200

    Import bookings route to apps

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express()
 // Import the users router module
 import usersRouter from './routes/usersRoutes.js'
 import foodRouter from './routes/foodRoutes.js'
+import bookingsRouter from './routes/bookingsRoute.js'
 
 // Tell the app to use the user router
 app.use(usersRouter)
 app.use(foodRouter)
+app.use(bookingsRouter)
 
 // create routes
 app.get('/', (req, res) => {
