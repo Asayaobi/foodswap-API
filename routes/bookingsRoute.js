93b98518b1ed1db569e7bf0b0b1a1437da87901e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 22:06:14 +0200

    Create bookingsRoute.js

---
+++ routes/bookingsRoute.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+const router = Router()
+
+// Define a GET route for fetching the list of booking
+router.get('/bookings', (req, res) => {
+  const booking = [
+    { id: 1, name: 'Food A', price: 100 },
+    { id: 2, name: 'Food B', price: 150 }
+    // Add more products as needed
+  ]
+  res.json(booking)
+})
+
+// Define a GET route for fetching a single dish
+router.get('/booking/1', (req, res) => {
+  res.send('Booking number 1')
+})
+
+// Export the router
+export default router
