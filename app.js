31cd6792da1788eaba5ef2ccbc0597c12a870340
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 22:15:19 +0200

    Import auth routes to app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import foodRouter from './routes/foodRoutes.js'
 import bookingsRouter from './routes/bookingsRoute.js'
 import reviewsRouter from './routes/reviewsRoute.js'
 import imagesRouter from './routes/imagesRoute.js'
+import authRouter from './routes/authRoute.js'
 
 // Tell the app to use the imported routers
 app.use(usersRouter)
@@ -16,6 +17,7 @@ app.use(foodRouter)
 app.use(bookingsRouter)
 app.use(reviewsRouter)
 app.use(imagesRouter)
+app.use(authRouter)
 
 // create routes
 app.get('/', (req, res) => {
