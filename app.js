92c7674ff36235c28d5102e4299b377d8ca2bfd4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 21:46:52 +0200

    update app.js

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hello Javascript!')
 })
 
+//for testing purpose
+app.get('/products', (req, res) => {
+  const products = [
+    { id: 1, name: 'Product A', price: 100 },
+    { id: 2, name: 'Product B', price: 150 }
+    // Add more products as needed
+  ]
+  res.send(products)
+})
+
 // keep the server open
 app.listen(4000, () => {
   console.log('Server is ready to accept requests')
