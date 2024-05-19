5400a1dc8588ef58a72de721c1a17c509a8edbd5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-19 23:13:56 +0200

    testing connection

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express'
+import db from '../db.js'
 const router = Router()
 
 // Define a GET route for fetching the list of users
-router.get('/users', (req, res) => {
-  res.send('List of users')
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM users')
+    console.log('rows', rows)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 // Define a GET route for fetching a single user
