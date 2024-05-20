0e07847f382d88d6c949e91586129c6c1ad5be55
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-20 20:52:29 +0200

    add post request for login in auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -23,8 +23,24 @@ router.post('/signup', async (req, res) => {
   }
 })
 
-router.get('/login', (req, res) => {
-  res.send('Login user')
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      throw new Error('Either your email or your password is missing')
+    } else {
+      const { rows } = await db.query(
+        `SELECT * FROM users 
+        WHERE email = '${email}' 
+        AND password = '${password}'`
+      )
+      console.log('log in rows', rows[0])
+      res.json(rows[0])
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.json(err.message)
+  }
 })
 
 router.get('/logout', (req, res) => {
