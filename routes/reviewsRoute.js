3492a3a03c184a70552a20e7e1dbe0f630061857
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-20 22:10:26 +0200

    Add post request to /reviews in reviews route

--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -2,6 +2,27 @@ import { Router } from 'express'
 import db from '../db.js'
 const router = Router()
 
+//Post a review for a dish
+router.post('/reviews', async (req, res) => {
+  try {
+    const { reviewer_id, food_id, review_text, rating, review_date } = req.body
+    if (!reviewer_id || !food_id || !rating) {
+      throw new Error('Either user_id, food_id, rating is missing.')
+    } else {
+      const review = await db.query(
+        `INSERT INTO reviews(reviewer_id, food_id, review_text, rating, review_date)
+      VALUES(${reviewer_id},${food_id},'${review_text}', ${rating},'${review_date}')
+      RETURNING *`
+      )
+      console.log('review response', review.rows[0])
+      res.json(review.rows[0])
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
+
 // Define a GET route for fetching the list of reviews
 router.get('/reviews', async (req, res) => {
   try {
