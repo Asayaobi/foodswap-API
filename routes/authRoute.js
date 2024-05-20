d1f9b589023fa4bbea60d6ac711e8b29d180fcb6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-20 20:28:09 +0200

    update post request for signup in auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,26 @@
 import { Router } from 'express'
+import db from '../db.js'
 const router = Router()
 
-router.get('/signup', (req, res) => {
-  res.send('Signup user')
+router.post('/signup', async (req, res) => {
+  try {
+    const { firstname, lastname, email, password, profile_image, city } =
+      req.body
+    if (!firstname || !lastname || !email || !password) {
+      throw new Error(
+        'Either first name, last name, email, password is missing.'
+      )
+    } else {
+      const { rows } = await db.query(
+        `INSERT INTO users (firstname, lastname, email, password, profile_image, city) VALUES ('${firstname}','${lastname}','${email}','${password}','${profile_image}','${city}') RETURNING *`
+      )
+      res.json(rows)
+      console.log('response', rows)
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 router.get('/login', (req, res) => {
