81aa0769f848de51d71d3044bc1e31e6eed0b4a5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-20 22:12:36 +0200

    Add post request block to /images in images route

--- routes/imagesRoute.js
+++ routes/imagesRoute.js
@@ -1,7 +1,14 @@
 import { Router } from 'express'
 import db from '../db.js'
 const router = Router()
-
+//Post images for food
+router.post('/images', async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
+})
 // Define a GET route for fetching the list of images
 router.get('/images', async (req, res) => {
   try {
