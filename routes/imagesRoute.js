bdf44695ff982614873df68d998be99e33f68044
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-26 00:05:35 +0200

    update query for patch food/foodId

--- routes/imagesRoute.js
+++ routes/imagesRoute.js
@@ -68,33 +68,26 @@ router.get('/images', async (req, res) => {
 router.patch('/images/:imageId', async (req, res) => {
   try {
     //validate token
-    const decoded = jwt.verify(req.cookies.jwt, jwtSecret)
-    console.log('decoded token', decoded)
-    if (!decoded.user_id || !decoded.email) {
+    const decodedToken = jwt.verify(req.cookies.jwt, jwtSecret)
+    console.log('decoded token', decodedToken)
+    if (!decodedToken.user_id || !decodedToken.email) {
       throw new Error('Invalid authentication token')
     }
     //check if this item belongs to this user_id
-    const { food_id, url } = req.body
+    const food_id = req.body.food_id
     const userCheck = await db.query(
       `SELECT * FROM food WHERE chef_id = ${decodedToken.user_id} AND food_id = ${food_id}`
     )
     if (userCheck.rowCount === 0) {
       throw new Error('You are not authorized.')
     } else {
-      let query = `UPDATE images SET `
-      let setArray = []
-      if (food_id) {
-        setArray.push(`food_id = ${food_id}`)
-      }
-      if (url) {
-        setArray.push(`url = '${url}'`)
-      }
-      query += setArray.join(',')
-      query += ` WHERE image_id = ${req.params.imageId} RETURNING *`
+      //updateImage
+      const url = req.body.url
+      const query = `UPDATE images SET url = '${url}' WHERE image_id = ${req.params.imageId} RETURNING *`
       console.log('query', query)
       const updateImage = await db.query(query)
-      console.log('update Image', updateImage.rows[0])
-      res.json(updateImage.rows[0])
+      console.log('update Image', updateImage.rows)
+      res.json(updateImage.rows)
     }
   } catch (err) {
     console.error(err.message)
