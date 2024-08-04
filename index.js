3fe1c3c2f3aeee72e7e9c0e5a1de6e571b8b39e5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-04 22:42:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import 'dotenv/config'
 // run the express function
 const app = express()
 
+// Middleware to parse JSON bodies
+app.use(express.json())
+app.use(cookieParser())
+
 // CORS setting
 app.use(
   cors({
@@ -22,10 +26,6 @@ import bookingsRouter from './routes/bookingsRoute.js'
 import reviewsRouter from './routes/reviewsRoute.js'
 import imagesRouter from './routes/imagesRoute.js'
 import authRouter from './routes/authRoute.js'
-
-// Middleware to parse JSON bodies
-app.use(express.json())
-app.use(cookieParser())
 
 // Tell the app to use the imported routers
 app.use(usersRouter)
